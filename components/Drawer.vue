9ff06e2163c050d68ceff3ac5dc66144a8a974fc
topty <topty@live.com>
2019-11-15 21:42:34 +0700

    Init drawer

---
+++ components/Drawer.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-navigation-drawer
+    fixed
+    app
+    dark
+    permanent
+    color="#23374D"
+  >
+    <div class="logo">
+      LWS
+    </div>
+
+    <v-list style="margin-top: 28px;">
+      <v-list-item
+        v-for="(item, i) in items"
+        :key="'menu-' + i"
+        :to="item.to"
+        link
+        class="drawer-menu"
+      >
+        {{ item.title }}
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <div class="copyright">
+        Lamo Web Service © 2019
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        title: 'Cloud Service',
+        to: '/'
+      },
+      {
+        title: 'Profile',
+        to: '/profile'
+      },
+      {
+        title: 'Billing'
+      },
+      {
+        title: 'Support'
+      }
+    ]
+  })
+}
+</script>
+
+<style>
+.logo {
+  font-family: Roboto;
+  font-size: 28px;
+  color: #ffffff;
+  margin-left: 64px;
+}
+.drawer-menu {
+  padding-left: 64px;
+}
+.copyright {
+  color: #DADADA;
+  text-align: center;
+  margin-bottom: 24px;
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,7 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
+    <drawer />
     <v-app-bar
-      :clipped-left="clipped"
       fixed
       app
     >
@@ -62,34 +38,16 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import Drawer from '@/components/Drawer'
+
 export default {
+  components: {
+    Drawer
+  },
   data () {
     return {
       clipped: false,
