9b7483629686bd1bbd763da3113367deec226a99
topty <topty@live.com>
2019-11-18 21:07:12 +0700

    Init invoices table

---
+++ pages/billing/invoices.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <tr>
+          <th
+            v-for="header in headers"
+            :key="'header-' + header.key"
+            class="text-center"
+          >
+            {{ header.name }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in invoices" :key="item.id">
+          <td
+            v-for="header in headers.slice(0, -1)"
+            :key="['invoice', item.id, header.key].join('-')"
+            class="text-center"
+          >
+            {{ item[header.key] }}
+          </td>
+          <td>
+            <a href="#" text color="primary" class="text-center">
+              Download
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    headers: [
+      {
+        name: 'ID',
+        key: 'id'
+      },
+      {
+        name: 'Status',
+        key: 'status'
+      },
+      {
+        name: 'Type',
+        key: 'type'
+      },
+      {
+        name: 'Description',
+        key: 'desc'
+      },
+      {
+        name: 'Date',
+        key: 'date'
+      },
+      {
+        name: 'Amount',
+        key: 'amount'
+      },
+      {
+        name: '',
+        key: 'download'
+      }
+    ],
+    invoices: [
+      {
+        id: '0',
+        status: 'Success',
+        type: 'Payment',
+        desc: 'Payment',
+        date: 'May 23, 2019',
+        amount: '$8.03'
+      },
+      {
+        id: '1',
+        status: 'Success',
+        type: 'Payment',
+        desc: 'Payment',
+        date: 'May 23, 2019',
+        amount: '$8.03'
+      }
+    ]
+  })
+}
+</script>
