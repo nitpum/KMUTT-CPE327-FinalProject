186ca56faaa37fcddb4f2e0196bddd0f88a438fa
topty <topty@live.com>
2019-11-23 17:24:50 +0700

    Initial choose an image

--- components/cloud/title/Dashboard.vue
+++ components/cloud/title/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-flex justify-between">
     <h1 class="headline">Dashboard</h1>
-    <v-btn class="ml-auto" color="primary" depressed large>
+    <v-btn class="ml-auto" color="primary" depressed large to="/cloud/create">
       Create Service
     </v-btn>
   </div>
---
+++ pages/cloud/create.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <plan />
+  </div>
+</template>
+
+<script>
+import Plan from '@/components/cloud/create/Plan'
+
+export default {
+  components: {
+    Plan
+  }
+}
+</script>
