e1ccc828ea234bc615968b782a995f9fe93f1162
topty <topty@live.com>
2019-11-24 14:18:26 +0700

    Add select an optional

---
+++ components/cloud/create/Optional.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="title">
+      Select an optional
+    </div>
+    <v-row>
+      <v-col 
+        v-for="(option, i) in options"
+        :key="'option-' + i"
+        cols="12"
+        sm
+      >
+        <v-checkbox
+          v-model="selected"
+          color="primary"
+          :label="option"
+          :value="option"
+          hide-details
+        />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    options: [
+      'Private networking',
+      'IPv6',
+      'Monitoring'
+    ],
+    selected: []
+  })
+}
+</script>
--- pages/cloud/create.vue
+++ pages/cloud/create.vue
@@ -3,6 +3,7 @@
     <images />
     <plans class="mt-5" />
     <regions class="mt-5" />
+    <optional class="mt-5" />
   </div>
 </template>
 
@@ -10,12 +11,14 @@
 import Images from '@/components/cloud/create/Images'
 import Plans from '@/components/cloud/create/Plans'
 import Regions from '@/components/cloud/create/Regions'
+import Optional from '@/components/cloud/create/Optional'
 
 export default {
   components: {
     Images,
     Plans,
-    Regions
+    Regions,
+    Optional
   }
 }
 </script>
