2e03e0f4a8e4f0bcce61e02eebf97775982f5cc9
topty <topty@live.com>
2019-11-28 23:01:00 +0700

    Init service page

---
+++ components/cloud/service/Destroy.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-card outlined>
+        <v-card-title>Destory Service</v-card-title>
+        <v-card-text>
+          This is <span class="text--primary">irrevisble</span>.
+          We will destroy your Service and all associated backups. All Service data will be scrubbed and irretrievable.
+        </v-card-text>
+        <v-card-text>
+          <v-btn color="error" outlined large>
+            Destroy
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
---
+++ components/cloud/service/Power.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-card outlined>
+        <v-card-title>Turn off Service</v-card-title>
+        <v-card-text>
+          When you turn off a Cloud service from the control panel, we first try a graceful shutdown. If that fails, we do a forced shutdown, which may corrupt data
+        </v-card-text>
+        <v-card-text>
+          When a Service off: <br /><br />
+
+          - Its data and IP address are retained its disk, CPU and RAM are reserved<br />
+          - You continue to accure its data transfer allowance.
+        </v-card-text>
+        <v-card-text>
+          <v-icon color="error">
+            mdi-alert
+          </v-icon>
+          <b class="text--primary">Warning</b>:
+          You still be
+          <span class="text--primary">billed</span>
+          for a turned off Service. To end billing destroy instead
+        </v-card-text>
+        <v-card-text>
+          <v-btn depressed large>
+            Turn off
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+    <v-col cols="12">
+      <v-card outlined>
+        <v-card-title>Power cycle</v-card-title>
+        <v-card-text>
+          A ower cycle will imediately hard reset the server. You should only choose this option when you are unable to reboot the Service from the comandline
+        </v-card-text>
+        <v-card-text>
+          <v-btn depressed large>
+            Power Cycle
+          </v-btn>
+        </v-card-text>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
