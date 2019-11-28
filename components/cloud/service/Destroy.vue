4da63f39287123754f9f00969650673758df2911
topty <topty@live.com>
2019-11-28 23:16:15 +0700

    Destroy dialog

--- components/cloud/service/Destroy.vue
+++ components/cloud/service/Destroy.vue
@@ -8,11 +8,50 @@
           We will destroy your Service and all associated backups. All Service data will be scrubbed and irretrievable.
         </v-card-text>
         <v-card-text>
-          <v-btn color="error" outlined large>
-            Destroy
-          </v-btn>
+          <v-dialog v-model="dialog" width="400">
+            <template #activator="{ on }">
+              <v-btn v-on="on" color="error" outlined large>
+                Destroy
+              </v-btn>
+            </template>
+            <v-card>
+              <v-card-title class="d-flex mb-3">
+                Destroy Service
+                <v-icon class="ml-auto" @click="dialog = false">
+                  mdi-close
+                </v-icon>
+              </v-card-title>
+              <v-card-text>
+                Are you sure would like to permanetly delete Ubuntu-s-1vcpu-1gb-sg1-01 ?
+                <br /><br />
+                Snapshots created from this Service will not be deleted and will continue to be charged. Snapshots must be deleted seperatedly.
+              </v-card-text>
+              <v-card-text>
+                <v-row>
+                  <v-col cols="6">
+                    <v-btn block depressed @click="dialog = false">
+                      cancel
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="6">
+                    <v-btn block depressed color="error">
+                      confirm
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
         </v-card-text>
       </v-card>
     </v-col>
   </v-row>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false
+  })
+}
+</script>
