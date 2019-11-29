f95ebfedad6771f0ff94db0414b36cee275203c7
topty <topty@live.com>
2019-11-29 21:19:05 +0700

    Add profile security layout

---
+++ pages/profile/security.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <sign-in-method />
+    </v-col>
+    <v-col cols="12">
+      <login-log />
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import SignInMethod from '@/components/profile/SignInMethod'
+import LoginLog from '@/components/profile/LoginLog'
+
+export default {
+  components: {
+    SignInMethod,
+    LoginLog
+  }
+}
+</script>
