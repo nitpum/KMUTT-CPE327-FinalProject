6240eb9195763bc4451e7107007f3856ab8c592a
topty <topty@live.com>
2019-11-29 21:03:41 +0700

    Add email end deactivate acc layout

---
+++ components/profile/DeactivateAccount.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card class="d-flex align-center">
+    <div class="flex-grow-1">
+      <v-card-title>
+        Deactivate Account
+      </v-card-title>
+      <v-card-text>
+        This will remove your account and disable your account
+      </v-card-text>
+    </div>
+    <div class="flex-shrink-1">
+      <v-card-text>
+        <v-btn color="error" outlined large>
+          deactivate
+        </v-btn>
+      </v-card-text>
+    </div>
+  </v-card>
+</template>
---
+++ components/profile/EMail.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Email preferences
+    </v-card-title>
+    <v-card-text>
+      To manage what emails you get, visit the
+      <router-link to="/">
+        Subscriptions Preferences Center
+        <v-icon small>
+          mdi-open-in-new
+        </v-icon>
+      </router-link>
+    </v-card-text>
+  </v-card>
+</template>
