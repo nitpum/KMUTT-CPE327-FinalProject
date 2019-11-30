ebd8d35852ff9c487b29b2d2cecaffd899d4c50e
topty <topty@live.com>
2019-11-30 22:00:27 +0700

    Profile from vuex

--- components/profile/SignInMethod.vue
+++ components/profile/SignInMethod.vue
@@ -2,11 +2,13 @@
   <v-card>
     <v-card-title>Sign-in method</v-card-title>
     <v-card-text>
-      <div class="text--primary">Username</div>john_doe
+      <div class="text--primary">Username</div>
+      {{ username }}
     </v-card-text>
     <v-card-text class="d-flex align-center">
       <div>
-        <div class="text--primary">Google Sign-in</div>john_doe@gmail.com
+        <div class="text--primary">Google Sign-in</div>
+        {{ googleSignin }}
       </div>
       <div class="ml-auto">
         <v-btn class="btn" color="primary" small depressed>add</v-btn>
@@ -15,7 +17,8 @@
     <v-divider />
     <v-card-text class="d-flex align-center">
       <div>
-        <div class="text--primary">Password</div>********
+        <div class="text--primary">Password</div>
+        ********
       </div>
       <div class="ml-auto">
         <change-password />
@@ -31,11 +34,18 @@
 </style>
 
 <script>
+import { mapState } from 'vuex'
 import ChangePassword from './ChangePassword'
 
 export default {
   components: {
     ChangePassword
+  },
+  computed: {
+    ...mapState({
+      username: state => state.profile.username,
+      googleSignin: state => state.profile.googleSignin
+    })
   }
 }
 </script>
