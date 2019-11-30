114e874145c0e0db5cbbfcde750b275a0117453e
yee2542 <yee2542gtl@gmail.com>
2019-11-30 21:37:51 +0700

    bind sign out to logout btn and add line

--- components/UserAvatar.vue
+++ components/UserAvatar.vue
@@ -1,37 +1,36 @@
 <template>
-  <v-menu
-    v-model="menu"
-    :close-on-content-click="false"
-    :nudge-width="150"
-    offset-y
-  >
+  <v-menu v-model="menu" :close-on-content-click="false" :nudge-width="150" offset-y>
     <template v-slot:activator="{ on }">
       <v-btn v-on="on" text>
         <v-avatar size="36px">
           <v-img :src="$store.state.profile.avatar" />
         </v-avatar>
-        <v-icon right>
-          {{ menu ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
-        </v-icon>
+        <v-icon right>{{ menu ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
       </v-btn>
     </template>
 
     <v-card>
       <v-list dense>
         <v-list-item>
-          <v-list-item-title>
-            Send feedback
-          </v-list-item-title>
+          <v-list-item-title>Send feedback</v-list-item-title>
         </v-list-item>
-        <v-list-item>
-          <v-list-item-title>
-            Logout
-          </v-list-item-title>
-        </v-list-item>
+        <div class="line"></div>
+        <router-link to="/">
+          <v-list-item>
+            <v-list-item-title>Logout</v-list-item-title>
+          </v-list-item>
+        </router-link>
       </v-list>
     </v-card>
   </v-menu>
 </template>
+
+<style scoped>
+.line {
+  width: 100%;
+  border-bottom: 0.5px solid rgba(102, 102, 102, 0.5);
+}
+</style>
 
 <script>
 export default {
