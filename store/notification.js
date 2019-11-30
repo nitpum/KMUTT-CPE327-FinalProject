171091157fee5f39a0a549a4d6697dc24eb0a534
topty <topty@live.com>
2019-11-30 21:40:04 +0700

    Noti with vuex

---
+++ store/notification.js
@@ -0,0 +1,14 @@
+export const state = () => ({
+  items: [
+    {
+      title: 'System',
+      body: 'Sakhon Nakhon 1 will down todat due to blackout',
+      time: 'November 27, 2019 21:09'
+    },
+    {
+      title: 'System',
+      body: 'ubuntu-s-1vcpu-1gb-sg1 is ready',
+      time: 'November 27, 2019 21:09'
+    }
+  ]
+})
